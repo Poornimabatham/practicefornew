e0fe82ad5da0f1e3aa8c305b9e374f4cba269d90
ubitechdevelopers <ubitechdeveloper@ubitechsolutions.com>
2023-08-09 17:25:16 +0530

    getplanner

--- app/Routes/Report/dist/GetplannerRoute.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Route_1 = require("@ioc:Adonis/Core/Route");
-Route_1["default"].group(function () {
-    Route_1["default"].get('/fetchsummary', 'GetplannerController.data');
-}).namespace('App/Controllers/Http/ReportController');
