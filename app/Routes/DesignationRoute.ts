97592581a634ad6c8f7e54283a24c00d8da1919c
ubitechparth <paarthbhansali@gmail.com>
2023-08-18 18:18:37 +0530

    CHECKED

--- app/Routes/DesignationRoute.ts
+++ app/Routes/DesignationRoute.ts
@@ -1,6 +1,6 @@
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/fetch', 'DesignationsController.retreiveDesign')
-Route.post('/add', 'DesignationsController.AddDesign')
-Route.put('/update', 'DesignationsController.UpdateDesign')
+Route.get('/fetchDesignations', 'DesignationsController.retreiveDesign')
+Route.post('/addDesignations', 'DesignationsController.AddDesign')
+Route.put('/updateDesignations', 'DesignationsController.UpdateDesign')
 
