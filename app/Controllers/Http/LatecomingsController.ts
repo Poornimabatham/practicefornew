3c997176a8b55f310976f6ec75ee93cc37467c8a
Mayank Sharma <132138966+Mayank26061993@users.noreply.github.com>
2023-08-31 15:28:44 +0530

    clear

--- app/Controllers/Http/LatecomingsController.ts
+++ app/Controllers/Http/LatecomingsController.ts
@@ -2,10 +2,19 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import LateComingService from 'App/Services/LateComingService'
 import LatecomingValidator from 'App/Validators/LatecomingValidator'
 export default class LatecomingsController {
-    public async FetchLateComings({request,response  }: HttpContextContract) {
-    const a = await  request.validate(LatecomingValidator.fetchlatecomingsschema)
-    const b = await  LateComingService.getLateComing(a)
-    return  response.json(b)
-    
-    }
+  public async FetchLateComings({ request, response }: HttpContextContract) {
+    const a = await request.validate(
+      LatecomingValidator.fetchlatecomingsschema
+    );
+    const b = await LateComingService.getLateComing(a);
+    return response.json(b);
+  }
+
+  public async getlateComingsCsv({ request, response }: HttpContextContract) {
+    const a = await request.validate(
+      LatecomingValidator.getlateComingsCsvSchema
+    );
+    const b = await LateComingService.getLateComingCsv(a);
+    return response.json(b);
+  }
 }
--- app/Routes/LateComingRoute.ts
+++ app/Routes/LateComingRoute.ts
@@ -1,3 +1,5 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/FetchLateComings','LatecomingsController.FetchLateComings')
+
+Route.get("/getlateComingsCsv", "LatecomingsController.getlateComingsCsv");
