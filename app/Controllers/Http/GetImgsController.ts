5d70ec2a619d37c95c775f4584c85ce99468895d
Ashish <ashish@ubitechsolutions.com>
2023-09-07 13:05:28 +0530

    pending for aws function getPresignedURL

--- app/Controllers/Http/GetImgsController.ts
+++ app/Controllers/Http/GetImgsController.ts
@@ -1,6 +1,6 @@
- import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import GetAttActualImgService from 'App/Services/GetAttActualImg';
- import GetImgValidator from 'App/Validators/GetImgValidator';
+import GetImgValidator from 'App/Validators/GetImgValidator';
 export default class GetImgsController {
     private data: any = [];
     public async GetAttActualImgs({ request, response }: HttpContextContract) {
@@ -12,8 +12,28 @@ export default class GetImgsController {
                 this.data['empId']=validation.empId?validation.empId:0;
                 this.data['date']=validation.date    
                 const result = await GetAttActualImgService.prototype.getImg(this.data);
+                if(result.length > 0){
+                    response.status(200).send(result);
+                }
+        }catch(err){
+            console.log(err)
 
+        }
 
+    }
+    public async GetVisitActualImgs({ request, response }: HttpContextContract) {
+        try{
+            //console.log(request.all())
+            const validation = await request.validate(GetImgValidator.getImg)
+                this.data['imgObj']=validation.imgObj?validation.imgObj:'';
+                this.data['orgId']=validation.orgId
+                this.data['empId']=validation.empId?validation.empId:0;
+                this.data['date']=validation.date    
+                const result = await GetAttActualImgService.prototype.getImgForVisit(this.data); 
+                if(result.length > 0){
+                    response.status(200).send(result);
+                }
+                
         }catch(err){
             console.log(err)
 
--- app/Services/GetAttActualImg.ts
+++ app/Services/GetAttActualImg.ts
@@ -1,30 +1,77 @@
-import moment from "moment-timezone";  
-import Database from "@ioc:Adonis/Lucid/Database";  
-import Helper from "App/Helper/Helper";  
-  
+import moment from "moment";
+
   
 export default class GetAttActualImgService{ 
     public async getImg(reqdata:[]) { 
         const imgObj: string = reqdata['imgObj'];
-        const filename: string = imgObj.split('/').pop() || '';
+        const filename: string[]  = imgObj.split('/');
+        let orgId:number=0;
         let bucketForlder: string;
-        let imgEmpId: string;
-        let newImgUrlObj: string;
+        let imgEmpId: string=filename[5];
+        let newImgUrlObj: string='';
+        let date:string= moment(reqdata['date']).format('YYYY-MM-DD');
         if (imgObj.includes('/public')) {
             bucketForlder = 'public/attendance_images';
-            imgEmpId = filename.split('_')[1];
+            imgEmpId = filename[6];
+            orgId=Number(filename[5]);
         } else {
             bucketForlder = 'attendance_images';
-            imgEmpId = filename.split('_')[0];
+            imgEmpId = filename[5];
+            orgId=Number(filename[4]);
         }
-        const useThis: string = filename.split('?')[0];
-        const conditionAccordingToMultiOrSingleShift: string[] = useThis.split('_');
-        if (conditionAccordingToMultiOrSingleShift[0] === 'GrpQR') {
-            newImgUrlObj = bucketForlder + "/" + reqdata['orgId'] + "/"+ imgEmpId + "/" + useThis;
+
+        if(imgObj){
+            const imgObjParts: string[] = imgObj.split('Thumbnail_').slice(-1);
+            const imgObjNew: string = imgObjParts.join("Thumbnail_"); 
+            const str2: string[] = imgObjNew.split('?');
+            const useThis: string = str2[0];
+            console.log(useThis);
+            //return false;
+            const conditionAccordingToMultiOrSingleShift: string[] = useThis.split('_');
+            if (conditionAccordingToMultiOrSingleShift[0] === 'GrpQR') {
+                newImgUrlObj = bucketForlder + "/" + orgId + "/"+ imgEmpId + "/" + useThis;
+            } else {
+                newImgUrlObj =  bucketForlder + "/" + orgId + "/"+ imgEmpId + "/" + date + "/" + useThis;
+            }
+        }
+       // console.log(newImgUrlObj);
+        return (process.env.ATTENDANCEBUCKET,newImgUrlObj);
+        //use Helper Function getPresignedURL
+        // $data['img']=getPresignedURL(process.env.ATTENDANCEBUCKET,newImgUrlObj);
+        // console.log(reqdata);
+        //return true;
+
+    }
+    public async getImgForVisit(reqdata:[]) { 
+        const imgObj: string = reqdata['imgObj'];
+        const imgArray: string[] = imgObj.split('/');
+        let imgEmpId: string;
+        let bucketForlder: string;
+        let newImgUrlObj: string='';
+
+        if (imgObj.includes('/public')) {
+            imgEmpId = imgArray[6];
+            bucketForlder = 'public/visits/'+ reqdata['orgId']+'/'+imgEmpId+'/';
         } else {
-            newImgUrlObj =  bucketForlder + "/" + reqdata['orgId'] + "/"+ imgEmpId + "/" + useThis;
+            imgEmpId = imgArray[5];
+            bucketForlder = 'visits/'+reqdata['orgId']+'/'+ imgEmpId+ '/';
+        }
+
+        if(imgObj){
+            const imgObjParts: string[] = imgObj.split('Thumbnail_').slice(-1);
+            const imgObjNew: string = imgObjParts.join("Thumbnail_"); 
+            const str2: string[] = imgObjNew.split('?');
+            const useThis: string = str2[0];
+            const conditionAccordingToMultiOrSingleShift: string[] = useThis.split('_');
+            if (conditionAccordingToMultiOrSingleShift[0] === 'GrpQR') {
+                newImgUrlObj = bucketForlder + useThis;
+            } else {
+                newImgUrlObj =  bucketForlder + useThis;
+            }
+            
         }
         return (process.env.ATTENDANCEBUCKET,newImgUrlObj);
+        //use Helper Function getPresignedURL
         // $data['img']=getPresignedURL(process.env.ATTENDANCEBUCKET,newImgUrlObj);
         // console.log(reqdata);
         //return true;
