ed3e1b4d44751e9dadc4ec4706874889fbc31a73
omchauhan2 <omc0784@gmail.com>
2023-09-12 16:56:23 +0545

    SendNotificationDiffOrgEmployee_12/sep_OM

--- app/Services/SendNotificationDiffOrgEmployee.ts
+++ app/Services/SendNotificationDiffOrgEmployee.ts
@@ -13,14 +13,12 @@ export default class SendNotificationDiffOrgEmployee {
         var adminName = await Helper.getAdminNamebyOrgId(orgid);
         var result: {} = {};
 
-        var selectQuery = await Database.from('UserMaster as U').select('U.EmployeeId', 'U.OrganizationId', 'U.appSuperviserSts', 'U.Username').where('U.username_mobile', encryptContact)
-
-        // return selectQuery
+        var selectQuery = await Database.from('UserMaster as U').select('U.EmployeeId', 'U.OrganizationId', 'U.appSuperviserSts', 'U.Username').whereRaw(` U.username_mobile like '%${encryptContact}%'`)
 
         if (selectQuery.length > 0) {
             let empid = selectQuery[0].EmployeeId;
             let Orgid = selectQuery[0].OrganizationId;
-            let username = selectQuery[0].Username;
+            let username = await Helper.decode5t(selectQuery[0].Username);
             let empName = await Helper.getEmpName(empid);
             empName = empName.replace(/\b\w/g, function (match) {
                 return match.toUpperCase();
@@ -29,26 +27,41 @@ export default class SendNotificationDiffOrgEmployee {
             empName = empName.replace(/[^A-Za-z0-9\-]/g, '');
             var employeeTopic = empName + empid;
             var NotificationId: any = await Helper.sendManualPushNotification(`${employeeTopic} in Topics}`, "Employee in another Organization", `You want to join this ${orgName} organization`, empid, Orgid, "HomePage");
-
-            var updateQuery = await Database.from('NotificationList').where('Id', NotificationId).update({ 'PageName': 'HomePage' })
+            var updateQuery = await Database.from('NotificationsList').where('Id', NotificationId).update({ PageName: 'HomePage' })
 
             result['status'] = 0;
-            if (updateQuery.length > 0) {
+            if (updateQuery) {
                 result['status'] = 1;
 
                 var selectPreventSignupQuery = await
-                    Database.from('PreventSignup').select('*').where('contact', contact).where('OrganizationId', Orgid);
+                    Database.from('PreventSignup').select('*').whereRaw(` contact like '%${contact}%'`).where('OrganizationId', Orgid);
 
                 if (selectPreventSignupQuery.length > 0) {
-                    var updatePreventsignupQuery = await Database.from('PreventSignup').whereNot('Status', 0).where('contact', contact).where('OrganizationId', Orgid);
+                    await Database.from('PreventSignup').whereNot('Status', 0).whereRaw(`contact like '%${contact}%'`).where('OrganizationId', Orgid).update({ Status: 0 });
                 }
                 else {
-                    var insertPreventSignupQuery = await Database.table('PreventSignup').insert({
-
+                    await Database.table('PreventSignup').insert({
+                        'EmployeeId': empid, 'contact': contact, 'OrganizationId': Orgid, 'OldOrgId': orgid, 'OldOrgName': orgName, 'Status': 0
                     })
                 }
-            }
 
+                var selectBodyQuery = await Database.from('All_mailers').select('Body', 'Subject').where('Id', 40);
+
+                if (selectBodyQuery.length > 0) {
+                    var body = selectBodyQuery[0].Body;
+                    var Subject = selectBodyQuery[0].Subject;
+                }
+
+                var Username = username + contact;
+                var body1 = body.replace('{Admin Name}', adminName);
+                var message_body = body1.replace('{Employee name(Number)}', Username, body1);
+                var headers = "MIME-Version: 1.0" + "\r\n";
+                headers = headers + "Content-type:text/html;charset=UTF-8" + "\r\n";
+                headers = headers + "From: <noreply@ubiattendance.com>" + "\r\n";
+                await Helper.sendEmail(adminEmail, Subject, message_body, headers)
+
+            }
+            return result;
         }
     }
 }
