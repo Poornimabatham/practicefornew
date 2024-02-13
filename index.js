cc3e0bc62d4b52e311a95bee3aff6896c8ee99b6
poornima <pbatham21@gmail.com>
2024-02-13 17:56:38 +0530

    js code

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ myFunction();
 
 function myFunction() {
   fruit = "apple"; // Considered global type is string
-//   fruit = // Considered global type is undefin
-    // fruit = 0; // Considered global type number
-    vegetables ='potato'; // Considered global type is
+  //   fruit = // Considered global type is undefin
+  // fruit = 0; // Considered global type number
+  vegetables = "potato"; // Considered global type is
   console.log(typeof petName + "- " + "My pet name is " + petName);
 }
 
@@ -17,3 +17,12 @@ console.log(
     "Fruit name is " +
     typeof vegetables
 );
+let a = ["shubam", "poornima", "Om", "manshi"];
+let results = []; // Array to store results
+
+for (let i = 0; i < a.length; i++) {
+  let result = a[i].split("").join("-");
+  results.push(result); // Store each result in the array
+}
+
+console.log(results); // Output: ['s-h-u-b-a-m', 'p-o-o-r-n-i-m-a', 'O-m', 'm-a-n-s-h-i']
