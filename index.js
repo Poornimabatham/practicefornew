c9836adc05febbcc34b16410362dc924e3562140
Poornimabatham <pbatham21@gmail.com>
2025-05-06 18:18:12 +0530

    AD

--- index.js
+++ index.js
@@ -18,70 +18,155 @@
 //  x = 90
 // // function testVarScope() {
 // //     x=8
-// //     console.log(x)
-// //     if (true) {
-// //        x = 10;
+// // //     console.log(x)
+// // //     if (true) {
+// // //        x = 10;
+// // //     }
+// // //     console.log(x); // ✅ 10 — accessible outside the block
+// // //   }
+  
+// //   // testVarScope();
+  
+// //   function outerFun(a) {
+// //     function innerFun(b) {
+// //         return a + b;
 // //     }
-// //     console.log(x); // ✅ 10 — accessible outside the block
-// //   }
-  
-//   // testVarScope();
-  
-//   function outerFun(a) {
-//     function innerFun(b) {
-//         return a + b;
-//     }
-//     return innerFun;
-// }
-
-// const addTen = outerFun(10);
-// // console.log(addTen(5));Output: 8
-
-
-// // Function calling
-// welcome();
-
-// console.log(A)  //why th avalue of A is not 9
-// var A =9
-// function welcome() {
-//    A =90
-// console.log(A)
-
-//   console.log("welcome to GfG");
-// }
-
-// //callin function 1. welcome()->print 90->undefined because var is hoisting on top 
-
-
-
-// let a = [2,2,4,6]
-
-// // let b = 0
-// // for(let i =0 ;i<a.length; i++){
-// //    b = b+a[i]
-
-
+// //     return innerFun;
 // // }
-// // console.log(b,"b")
 
-// const AB = [10, 20, 30, 40];  // example array
+// // const addTen = outerFun(10);
+// // // console.log(addTen(5));Output: 8
 
-// let key = 30
-// for (let i = AB.length; i >= 0; i--) {
-//   if(key ==AB[i]){
-//     AB[i] =(56)
 
-//   }
-//   AB.push(0)
-//   console.log(AB[i]);
+// // // Function calling
+// // welcome();
+
+// // console.log(A)  //why th avalue of A is not 9
+// // var A =9
+// // function welcome() {
+// //    A =90
+// // console.log(A)
+
+// //   console.log("welcome to GfG");
+// // }
+
+// // //callin function 1. welcome()->print 90->undefined because var is hoisting on top 
+
+
+
+// // let a = [2,2,4,6]
+
+// // // let b = 0
+// // // for(let i =0 ;i<a.length; i++){
+// // //    b = b+a[i]
+
+
+// // // }
+// // // console.log(b,"b")
+
+// // // const AB = [10, 20, 30, 40];  // example array
+
+// // // let key = 30
+// // // for (let i = AB.length; i >= 0; i--) {
+// // //   if(key ==AB[i]){
+// // //     AB[i] =(56)
+
+// // //   }
+// // //   AB.push(0)
+// // //   console.log(AB[i]);
+// // // }
+
+
+
+
+// // let a1 = [2,4,6,7]
+// // let b1 = [2,3,5,6,7]
+
+// // for(let i= 0;i<a1.length;i++){
+// //   console.log(a1[i]+b1[i])
+// // }
+
+
+// //shallow copy
+// // let  arr = [1,2,4,5]
+// // console.log(arr.reverse().push(8))  give the lenght of an array 
+// // let t1 = arr
+// // console.log(t1)
+
+// // t1[0] = 99
+// // console.log(arr[0])
+
+
+// //deep copy method
+// let arr3 = [1, 2, 3];    
+// let t1 = [...arr3];  // or arr.slice()
+
+// t1[0] = 99;
+// console.log(arr3); // [1, 2, 3] — original is safe
+
+
+
+
+// // let arr = [1, 2, 4, 5];
+// // let b  = arr.reverse();       // Now arr = [5, 4, 2, 1]
+// // b.push(8);         // Now arr = [5, 4, 2, 1, 8]
+// // console.log(b);    // [5, 4, 2, 1, 8]
+
+
+// //hosting
+
+// console.log(x)
+// var x =9
+
+// // console.log(b)
+
+// let b =9
+
+// greet()
+// function greet(){
+//   console.log("hello")
 // }
 
 
 
+a()
+b2()
+var t1=87
+function a(){
+  let t1 = 2
+  console.log(t1)
+}
+console.log(t1)
+function b2(){
+  let t2 = 6
+  console.log(t2)
+}
 
-let a1 = [2,4,6,7]
-let b1 = [2,3,5,6,7]
 
-for(let i= 0;i<a1.length;i++){
-  console.log(a1[i]+b1[i])
+const add = (a, b) => a + b;
+console.log(add(4))  //Nan
+
+
+console.log(typeof(6/"5"))
+
+
+let data = 7
+
+while(data>=0){
+  if(data<=5){
+    data--
+    continue
+  }
+  console.log(data)
+  data--
+}
+
+
+let i = 5
+while(true){
+  console.log(i)
+  i+=5
+  if(i>30){
+    break
+  }
 }
